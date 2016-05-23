20c279b36be817c23400c3aaf0fae7133bcfdae3
Hondy000 <kazu.1994.1014@gmail.com>
2016-05-23 15:34:21 +0900

    ファイル移動

--- UnityProject/Assets/DirtyCreater.cs
+++ UnityProject/Assets/DirtyCreater.cs
@@ -18,14 +18,20 @@ public class DirtyCreater : MonoBehaviour {
 
     GameObject[] dirtyObjectInstance;   // 管理用
 
-    bool is_myDityDestroy;  // 消されたかどうか
+    bool isMyDityDestroy;  // 消されたかどうか
+
+    bool isRangeOut;   // マップ範囲外であるか
+
+    char rangeOutCount;
+
+    float deltaTime;
 
     // Use this for initialization
     void Start ()
     {
         dirtyObjectInstance = new GameObject[createNumber];
         createTime = 0;
-        is_myDityDestroy = true;
+        isMyDityDestroy = true;
 
 #if DEBUG
         if (GetComponent<MeshRenderer>() != null)
@@ -46,46 +52,15 @@ public class DirtyCreater : MonoBehaviour {
 
     void DirtyDestroy()
     {
-        is_myDityDestroy = true;
+        isMyDityDestroy = true;
     }
 
     // Update is called once per frame
     void Update ()
     {
         DrawDebugQuadMesh();
-        // 汚れがなくて 生成時間時間が0
-        if (is_myDityDestroy && createTime <= 0)
-        {
-            for (int i = 0;i < createNumber; i++)
-            {
-                Vector3 pos =  new Vector3(Mathf.Cos( Mathf.Deg2Rad * i * 360.0f / (float)createNumber), Mathf.Sin(Mathf.Deg2Rad * i * 360.0f / (float)createNumber), 0);
-                pos.x *= createRange.x;
-                pos.y *= createRange.y;
-                pos = transform.rotation * pos;
-                dirtyObjectInstance[i] = (GameObject)Instantiate(dirtyObject, pos + transform.position, transform.rotation);
-
-                // デバッグ用 街範囲外や規定高度外(クリエイトポイント基準)な場合 警告
-#if DEBUG
-                if (pos.y < -0.5)
-                {
-                    Debug.Log(dirtyObjectInstance[i].name + "の生成場所が規定範囲を超えています!");
-                }
-#endif
-
-                // 親子関係的なものを構築
-                DirtyObjectScript obj = dirtyObjectInstance[i].GetComponent<DirtyObjectScript>();
-                dirtyObjectInstance[i].transform.parent = this.transform;
-                obj.MyCreater = this;   // 汚れスクリプトに自分を伝える
-                is_myDityDestroy = false;
-
-            }
-            createTime = 600;
-
-        }
-        else
-        {
-            createTime -= Time.deltaTime;
-        }
+        //
+        deltaTime = Time.deltaTime;
     }
 
 
@@ -104,5 +79,46 @@ public class DirtyCreater : MonoBehaviour {
         Debug.DrawLine(transform.position + line[1], transform.position + line[2], Color.red);
         Debug.DrawLine(transform.position + line[2], transform.position + line[3], Color.red);
         Debug.DrawLine(transform.position + line[3], transform.position + line[0], Color.red);
+    }
+
+    public void CheckDistance(Vector3 playerPosition)
+    {
+        float distance = Vector3.Distance(playerPosition, transform.position);
+        if (distance >= 15.0f)
+        {
+            isRangeOut = true;
+        }
+        else
+        {
+            isRangeOut = false;
+        }
+    }
+
+    public void CreateDirty()
+    {
+        for (int i = 0; i < createNumber; i++)
+        {
+            Vector3 pos = new Vector3(Mathf.Cos(Mathf.Deg2Rad * i * 360.0f / (float)createNumber), Mathf.Sin(Mathf.Deg2Rad * i * 360.0f / (float)createNumber), 0);
+            pos.x *= createRange.x;
+            pos.y *= createRange.y;
+            pos = transform.rotation * pos;
+            dirtyObjectInstance[i] = (GameObject)Instantiate(dirtyObject, pos + transform.position, transform.rotation);
+            
+            // 親子関係的なものを構築
+            DirtyObjectScript obj = dirtyObjectInstance[i].GetComponent<DirtyObjectScript>();
+            dirtyObjectInstance[i].transform.parent = this.transform;
+            obj.MyCreater = this;   // 汚れスクリプトに自分を伝える
+            isMyDityDestroy = false;
+
+        }
+        
+    } 
+
+    void CountTime()
+    {
+        if (isRangeOut)
+        {
+            rangeOutCount++;
+        }
     }
 }
---
+++ UnityProject/Assets/DirtySystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirtySystem : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    GameObject points;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        DirtyCreater creater =  points.GetComponent<DirtyCreater>();
+        creater.CheckDistance(player.transform.position);
+    }
+}
