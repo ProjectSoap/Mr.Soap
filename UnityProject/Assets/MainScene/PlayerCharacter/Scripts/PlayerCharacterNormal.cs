7747f250aef9faaeb7c51f23bba8fe8bb68755cc
SeaurchinIgaguri <seaurchin.uni.igaguri@gmail.com>
2016-06-06 01:11:21 +0900

    PlayerCharacter更新
    とりあえず結合
    ドリフトエフェクト追加

---
+++ UnityProject/Assets/MainScene/PlayerCharacter/Scripts/PlayerCharacterNormal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacterNormal : PlayerCharacterAnimationBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateFixedUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //m_soapDrive.SendMessage("Accel");
+        //m_soapDrive.SendMessage("Breake");
+        //m_soapDrive.SendMessage("Jump");
+    }
+}
