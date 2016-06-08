64eb548095916d3d099b48820e606509fbed6b49
Goraku03700 <goraku.like@gmail.com>
2016-06-08 23:29:00 +0900

    add Signed-off-by: Goraku03700 <goraku.like@gmail.com>

---
+++ UnityProject/Assets/Nakao/CharSelMgr.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharSelMgr : MonoBehaviour {
+    [SerializeField]
+    PauseUI charUI;
+    enum Charselect
+    {
+        SELECT,
+        PLAY
+    };
+
+    [SerializeField]
+    List<GameObject> soaps;
+
+    [SerializeField]
+    SelectCharacter soap;
+
+    Charselect state;
+	// Use this for initialization
+	void Start () {
+        state = Charselect.SELECT;
+        if (PlayerPrefs.GetInt("SekkenChanPlayFlg", -1) > 0)
+        {
+            soaps[1].active = false;
+        }
+        else
+        {
+            soaps[0].active = false;
+        }
+
+        if (PlayerPrefs.GetInt("SekkenKun0PlayFlg", -1) > 0)
+        {
+            soaps[3].active = false;
+        }
+        else
+        {
+            soaps[2].active = false;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Return))
+        {
+            switch(state)
+            {
+                case Charselect.SELECT:
+                    {
+                        if (CheckOpenChar(soap.GetCharNo()))
+                        {
+                            state = Charselect.PLAY;
+                            charUI.ChangePause();
+                        }
+                        break;
+                    }
+                case Charselect.PLAY:
+                    {
+//                        Application.LoadLevel("Main");
+                        Fade.ChangeScene("main");
+                        break;
+                    }
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            switch (state)
+            {
+                case Charselect.SELECT:
+                    {
+                        Fade.ChangeScene("Menu");
+                        break;
+                        
+                    }
+                case Charselect.PLAY:
+                    {
+                        state = Charselect.SELECT;
+                        charUI.ChangePause();
+                        break;
+                    }
+            }
+        }
+	}
+
+    bool CheckOpenChar(int no)
+    {
+        switch(no)
+        {
+            case 0 :
+                {
+                    return true;
+                }
+            case 1:
+                {
+                    if(soaps[2].active)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            case 2:
+                {
+                    if (soaps[0].active)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            default:
+                {
+                    return false;
+                }
+        }
+        return false;
+    }
+}
---
+++ UnityProject/Assets/Nakao/PauseMenuMode.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PauseMenuMode : MonoBehaviour {
+
+    public List<PauseObject> pauseMenu;
+    public int beforeMode;
+	// Use this for initialization
+	void Start () {
+        pauseMenu[1].PushPose();
+        pauseMenu[2].PushPose();
+	}
+	
+    public void ChangePauseMenu(int pauseState)
+    {
+        switch(pauseState)
+        {
+            case 1:
+                {
+                    if(beforeMode == 2)
+                    {
+                        pauseMenu[0].PushPose();
+                        pauseMenu[1].PushPose();
+                    }
+                    if (beforeMode == 3)
+                    {
+                        pauseMenu[0].PushPose();
+                        pauseMenu[2].PushPose();
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    pauseMenu[0].PushPose();
+                    pauseMenu[1].PushPose();
+                    beforeMode = pauseState;
+                    break;
+                }
+            case 3:
+                {
+                    pauseMenu[0].PushPose();
+                    pauseMenu[2].PushPose();
+                    beforeMode = pauseState;
+                    break;
+                }
+        }
+    }
+	
+}
