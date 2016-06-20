abcce1689267731d1d27550a986bc45f39ea64c0
Goraku03700 <goraku.like@gmail.com>
2016-06-20 20:05:03 +0900

    fack ikeuti Signed-off-by: Goraku03700 <goraku.like@gmail.com>

---
+++ UnityProject/Assets/Nakao/GameEndResolutionSet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEndResolutionSet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int width = 1280;
+        int height = 720;
+
+        bool fullscreen = false;
+
+        int preferredRefreshRate = 60;
+
+        Screen.SetResolution(width, height, fullscreen, preferredRefreshRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Nakao/GameMainResolutionSet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMainResolutionSet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int width = 960;
+        int height = 540;
+
+        bool fullscreen = false;
+
+        int preferredRefreshRate = 60;
+
+        Screen.SetResolution(width, height, fullscreen, preferredRefreshRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
