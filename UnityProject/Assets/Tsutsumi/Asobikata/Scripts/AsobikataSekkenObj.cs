72fdcae050fec9f6964a1d4f6eec5f0f50bdef76
TsutsumiShogo <1sion2de0susi2@gmail.com>
2016-06-21 18:40:58 +0900

    遊び方１～８の説明文実装 遊び方シーンUI関係実装

---
+++ UnityProject/Assets/Tsutsumi/Asobikata/Scripts/AsobikataSekkenObj.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsobikataSekkenObj : MonoBehaviour {
+
+    [SerializeField]
+    private List<Sprite> spriteList;
+
+    private SpriteRenderer render;
+
+	// Use this for initialization
+	void Awake () {
+        render = transform.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //nullじゃなかったらtrue返す
+    public bool isNotNullSprite(int spriteNo)
+    {
+        if(spriteNo < 0 || spriteNo >= spriteList.Count)
+            return false;
+        if (spriteList[spriteNo] == null)
+            return false;
+
+        //スプライトが存在する
+        return true;
+    }
+    //この関数の前に上のnullチェック関数を通すこと
+    public void SetSprite(int spriteNo)
+    {
+        render.sprite = spriteList[spriteNo];
+    }
+}
