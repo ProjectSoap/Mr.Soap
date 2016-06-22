ade4c25121aa614b6864d0420956f5164fd9d9c4
Hondy000 <kazu.1994.1014@gmail.com>
2016-06-22 17:27:02 +0900

    クレジットにてカメラの一時停止追加

--- UnityProject/Assets/HondyTestUnits/Credit/Script/CreditCameraStopper.cs
+++ UnityProject/Assets/HondyTestUnits/Credit/Script/CreditCameraStopper.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CreditCameraStopper : MonoBehaviour {
     CreditCameraController m_camera;
+	float m_tempVelocity;
+	public float m_stayTime = 2;
+	bool m_isCameraStay = false;
     // Use this for initialization
     void Start () {
         m_camera = GameObject.Find("Main Camera").GetComponent<CreditCameraController>();
@@ -10,10 +13,26 @@ public class CreditCameraStopper : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (m_isCameraStay)
+		{
+			m_stayTime -= Time.deltaTime;
+			if (m_stayTime <= 0)
+			{
+				m_camera.CamraVelocity = m_tempVelocity;
+				m_isCameraStay = false;
+			}
+		}
 	}
     void OnTriggerEnter(Collider coll)
     {
-        m_camera.CamraVelocity = 0;
-    }
+		if (coll.name == "Trigger")
+		{
+
+			float temp = 0;
+			m_tempVelocity = m_camera.CamraVelocity;
+
+			m_camera.CamraVelocity = 0;
+			m_isCameraStay = true;
+		}
+	}
 }
