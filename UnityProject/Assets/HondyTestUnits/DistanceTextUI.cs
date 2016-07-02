4f8dcb97d6f059023c3bc646b7911449858e473d
Hondy000 <kazu.1994.1014@gmail.com>
2016-07-03 03:17:58 +0900

    マーカーつくった

---
+++ UnityProject/Assets/HondyTestUnits/DistanceTextUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DistanceTextUI : MonoBehaviour {
+
+	Text m_myText;
+
+	[SerializeField]
+	GameObject m_targetA;
+
+	[SerializeField]
+	GameObject m_targetB;
+
+	float m_distance;
+	// Use this for initialization
+	void Start () {
+		m_myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		m_distance = (int)Vector3.Distance(m_targetA.transform.localPosition, m_targetB.transform.localPosition); 
+		m_myText.text = m_distance.ToString() + "メートル";
+	}
+}
